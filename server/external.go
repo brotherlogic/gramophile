b82c178abf40bdd2d49aa89a148e8f187ec67b8e
Simon Tucker <brotherlogic@gmail.com>
2023-03-26 22:59:21 -0700

    Log this

--- server/external.go
+++ server/external.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"log"
 	"os"
 
 	"github.com/brotherlogic/discogs"
@@ -10,6 +11,9 @@ import (
 )
 
 func (s *Server) GetURL(ctx context.Context, req *pb.GetURLRequest) (*pb.GetURLResponse, error) {
+
+	log.Printf("%v and %v and %v", os.Getenv("DISCOGS_KEY"), os.Getenv("DISCOGS_SECRET"), os.Getenv("DISCOGS_CALLBACK"))
+
 	d := discogs.DiscogsWithAuth(os.Getenv("DISCOGS_KEY"), os.Getenv("DISCOGS_SECRET"), os.Getenv("DISCOGS_CALLBACK"))
 	url, _, err := d.GetLoginURL()
 	return &pb.GetURLResponse{URL: url}, err
