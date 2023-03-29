0fda4a34b8743cf71689f5b4f32146a6230b6faa
Simon Tucker <brotherlogic@gmail.com>
2023-03-28 22:52:19 -0700

    Huh

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
-		log.Fatalf("gramophile is unable to listen on the main serving port %v: %v", *port, err)
+		log.Fatalf("gramophile is actually unable to listen on the main serving port %v: %v", *port, err)
 	}
 	gs := grpc.NewServer()
 	pb.RegisterGramophileEServiceServer(gs, s)
