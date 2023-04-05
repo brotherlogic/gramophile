8c0caccdef20455a931969ef8842469e96886c2d
Simon Tucker <brotherlogic@gmail.com>
2023-04-04 22:53:51 -0700

    Update

--- admin_cli/cli.go
+++ admin_cli/cli.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	pb "github.com/brotherlogic/gramophile/proto"
 )
 
 func main() {
-	conn, err := grpc.Dial(os.Args[1])
+	conn, err := grpc.Dial(os.Args[1], grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Bad dial: %v", err)
 	}
@@ -20,8 +22,9 @@ func main() {
 
 	switch os.Args[2] {
 	case "refresh":
-		client.Execute(context.Background(), &pb.ExecuteRequest{
-			Element: &pb.QueueElement{Token: os.Args[2], Secret: os.Args[3], Entry: &pb.QueueElement_RefreshUser{RefreshUser: &pb.RefreshUserEntry{Auth: os.Args[4]}}},
+		a, b := client.Execute(context.Background(), &pb.ExecuteRequest{
+			Element: &pb.QueueElement{Token: os.Args[3], Secret: os.Args[4], Entry: &pb.QueueElement_RefreshUser{RefreshUser: &pb.RefreshUserEntry{Auth: os.Args[4]}}},
 		})
+		fmt.Printf("%v and %v\n", a, b)
 	}
 }
