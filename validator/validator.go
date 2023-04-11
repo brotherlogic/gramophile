6fef09234b131990f8aae3abe34ede9365cf691d
Simon Tucker <brotherlogic@gmail.com>
2023-04-10 20:58:23 -0700

    Completes validation through deletion. This closes #44

--- validator/validator.go
+++ validator/validator.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"time"
 
 	"google.golang.org/grpc"
 
@@ -15,9 +16,31 @@ func validateUsers(ctx context.Context) error {
 		return err
 	}
 	client := pb.NewGramophileServiceClient(conn)
+	queue := pb.NewQueueServiceClient(conn)
 	users, err := client.GetUsers(ctx, &pb.GetUsersRequest{})
 	if err != nil {
 		return err
+	}
+
+	for _, user := range users.GetUsers() {
+		if time.Since(time.Unix(user.GetLastRefreshTime(), 0)) > time.Hour*24*7 {
+			client.DeleteUser(ctx, &pb.DeleteUserRequest{Id: user.GetAuth().GetToken()})
+		} else {
+			_, err := queue.Execute(ctx, &pb.ExecuteRequest{
+				Element: &pb.QueueElement{
+					RunDate:          time.Now().Unix(),
+					Token:            user.GetUserToken(),
+					Secret:           user.GetUserSecret(),
+					BackoffInSeconds: 10,
+					Entry: &pb.QueueElement_RefreshUser{
+						RefreshUser: &pb.RefreshUserEntry{Auth: user.GetAuth().GetToken()},
+					},
+				},
+			})
+			if err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
