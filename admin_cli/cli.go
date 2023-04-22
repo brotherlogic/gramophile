56039c1fa78873727bf04579d78eaf94e038a3dd
Simon Tucker <brotherlogic@gmail.com>
2023-04-21 22:48:32 -0700

    Fix queue deploy

--- admin_cli/cli.go
+++ admin_cli/cli.go
@@ -22,7 +22,7 @@ func main() {
 
 	switch os.Args[2] {
 	case "refresh":
-		a, b := client.Execute(context.Background(), &pb.EnqueueRequest{
+		a, b := client.Enqueue(context.Background(), &pb.EnqueueRequest{
 			Element: &pb.QueueElement{Auth: os.Args[3], Entry: &pb.QueueElement_RefreshUser{RefreshUser: &pb.RefreshUserEntry{Auth: os.Args[3]}}},
 		})
 		fmt.Printf("%v and %v\n", a, b)
