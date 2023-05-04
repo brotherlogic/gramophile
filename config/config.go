5db9617bad5609e7cda5cc2af317ecfef8a58ff6
Simon Tucker <brotherlogic@gmail.com>
2023-05-03 19:43:01 -0700

    Applies config to a record. This closes #118

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"time"
 
 	pb "github.com/brotherlogic/gramophile/proto"
 
@@ -13,4 +14,54 @@ func Hash(c *pb.GramophileConfig) string {
 	bytes, _ := proto.Marshal(c)
 	hash := md5.Sum(bytes)
 	return hex.EncodeToString(hash[:])
+}
+
+func setIssue(r *pb.Record, issue pb.NoncomplianceIssue, set bool) {
+	found := false
+	var newIssues []pb.NoncomplianceIssue
+	for _, existing := range r.GetIssues() {
+		if existing != issue {
+			newIssues = append(newIssues, existing)
+			found = true
+		}
+	}
+
+	if set && !found {
+		r.Issues = append(r.Issues, issue)
+	}
+
+	if !set {
+		r.Issues = newIssues
+	}
+}
+
+func filter(filter *pb.Filter, r *pb.Record) bool {
+	for _, format := range r.GetRelease().GetFormats() {
+		for _, matcher := range filter.GetFormats() {
+			if matcher == format.GetName() {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func Apply(c *pb.GramophileConfig, r *pb.Record) error {
+	if c.GetCleaningConfig().GetCleaning() != pb.Cleaning_NO_CLEANING && c.GetCleaningConfig().GetCleaning() != pb.Cleaning_CLEANING_UNKNOWN {
+		if filter(c.GetCleaningConfig().GetAppliesTo(), r) {
+			needsClean := false
+			if c.GetCleaningConfig().GetCleaningGapInSeconds() > 0 && time.Since(time.Unix(r.GetLastCleanTime(), 0)) > time.Second*time.Duration(c.CleaningConfig.GetCleaningGapInSeconds()) {
+				needsClean = true
+			}
+
+			if c.GetCleaningConfig().GetCleaningGapInPlays() > 0 && r.GetNumPlays() > c.GetCleaningConfig().GetCleaningGapInPlays() {
+				needsClean = true
+			}
+
+			setIssue(r, pb.NoncomplianceIssue_NEEDS_CLEAN, needsClean)
+		}
+	}
+
+	return nil
 }
