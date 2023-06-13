8805bfc227dc833bec20e9574800d008de9ba27d
Simon Tucker <brotherlogic@gmail.com>
2023-06-13 16:43:37 -0700

    Supports config to listen to. This closes #250

--- server/getrecord.go
+++ server/getrecord.go
@@ -3,12 +3,43 @@ package server
 import (
 	"context"
 	"math/rand"
+	"sort"
 	"time"
 
+	"github.com/brotherlogic/gramophile/config"
 	pb "github.com/brotherlogic/gramophile/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+func reverse(rs []*pb.Record) []*pb.Record {
+	for i := 0; i < len(rs)/2; i++ {
+		j := len(rs) - i - 1
+		rs[i], rs[j] = rs[j], rs[i]
+	}
+	return rs
+}
+
+func (s *Server) applyListeningFilter(ctx context.Context, f *pb.ListenFilter, rs []*pb.Record) *pb.Record {
+	switch f.GetOrder().GetOrdering() {
+	case pb.Order_ORDER_ADDED_DATE:
+		sort.SliceStable(rs, func(i, j int) bool {
+			return rs[i].GetRelease().GetInstanceId() < rs[j].GetRelease().GetInstanceId()
+		})
+	}
+
+	if f.GetOrder().GetReverse() {
+		reverse(rs)
+	}
+
+	for _, r := range rs {
+		if config.Filter(f.GetFilter(), r) {
+			return r
+		}
+	}
+
+	return nil
+}
 
 func (s *Server) GetRecord(ctx context.Context, req *pb.GetRecordRequest) (*pb.GetRecordResponse, error) {
 	u, err := s.getUser(ctx)
@@ -23,6 +54,36 @@ func (s *Server) GetRecord(ctx context.Context, req *pb.GetRecordRequest) (*pb.G
 
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(rids), func(i, j int) { rids[i], rids[j] = rids[j], rids[i] })
+
+	if req.GetGetRecordToListenTo() != nil && req.GetGetRecordToListenTo().GetFilter() != "" {
+		var filter *pb.ListenFilter
+		for _, f := range u.GetConfig().GetListenConfig().GetFilters() {
+			if f.GetName() == req.GetGetRecordToListenTo().GetFilter() {
+				filter = f
+				break
+			}
+		}
+		if filter == nil {
+			return nil, status.Errorf(codes.NotFound, "Unable to find a listening filter with name %v", req.GetGetRecordToListenTo().GetFilter())
+		}
+
+		var records []*pb.Record
+		for _, r := range rids {
+			r, err := s.d.GetRecord(ctx, u.GetUser().GetDiscogsUserId(), r)
+			if err != nil {
+				return nil, err
+			}
+
+			records = append(records, r)
+		}
+
+		ret := s.applyListeningFilter(ctx, filter, records)
+
+		if ret == nil {
+			return nil, status.Errorf(codes.NotFound, "Unable to locate record to listen to from %v", req.GetGetRecordToListenTo().GetFilter())
+		}
+		return &pb.GetRecordResponse{Record: ret}, nil
+	}
 
 	for _, rec := range rids {
 		r, err := s.d.GetRecord(ctx, u.GetUser().GetDiscogsUserId(), rec)
