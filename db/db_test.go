b8e10a8498fbcbe48943ccd0fbfcdd145f991ebf
brotherlogic <brotherlogic@gmail.com>
2023-08-26 23:44:50 -0700

    Added test file

---
+++ db/db_test.go
@@ -0,0 +1,33 @@
+package db
+
+import (
+	"testing"
+
+	pb "github.com/brotherlogic/gramophile/proto"
+)
+
+func TestUpdateDiff(t *testing.T) {
+	tests := []struct {
+		update  *pb.RecordUpdate
+		diffstr string
+	}{
+		{
+			update: &pb.RecordUpdate{
+				Before: &pb.Record{
+					GoalFolder: "",
+				},
+				After: &pb.Record{
+					GoalFolder: "12 Inches",
+				},
+			},
+			diffstr: "Goal Folder was set to 12 Inches",
+		},
+	}
+
+	for _, tc := range tests {
+		rd := resolveDiff(tc.update)
+		if rd != tc.diffstr {
+			t.Errorf("bad diff: expected %v, got %v", tc.diffstr, rd)
+		}
+	}
+}
