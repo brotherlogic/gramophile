d998c2a41a2b599fc5aa7460814ea4ce422cac30
brotherlogic <brotherlogic@gmail.com>
2023-08-29 16:08:31 -0700

    Adds missing files

---
+++ config/sleeve.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"context"
+	"fmt"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	SLEEVE_FIELD = "Sleeve"
+)
+
+type sleeve struct{}
+
+func (*sleeve) Validate(ctx context.Context, fields []*pbd.Field, c *pb.GramophileConfig) error {
+	if c.GetSleeveConfig().GetMandate() != pb.Mandate_NONE {
+		found := false
+		for _, field := range fields {
+			if field.GetName() == SLEEVE_FIELD {
+				found = true
+			}
+		}
+		if !found {
+			return status.Errorf(codes.FailedPrecondition, fmt.Sprintf("Add a field called '%v'", SLEEVE_FIELD))
+		}
+
+		if len(c.SleeveConfig.GetAllowedSleeves()) == 0 {
+			return status.Errorf(codes.FailedPrecondition, fmt.Sprintf("you must set at least one sleeve"))
+		}
+	}
+
+	return nil
+}
---
+++ config/sleeve_test.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"context"
+	"testing"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+)
+
+func TestSleeveFailed_NoField(t *testing.T) {
+	c := &pb.GramophileConfig{SleeveConfig: &pb.SleeveConfig{Mandate: pb.Mandate_RECOMMENDED,
+		AllowedSleeves: []*pb.Sleeve{{Name: "test"}}}}
+
+	err := ValidateConfig(context.Background(), []*pbd.Field{}, c)
+	if err == nil {
+		t.Errorf("Should have failed but did not")
+	}
+}
+
+func TestSleeveFailed_NoSleeves(t *testing.T) {
+	c := &pb.GramophileConfig{SleeveConfig: &pb.SleeveConfig{Mandate: pb.Mandate_RECOMMENDED}}
+
+	err := ValidateConfig(context.Background(), []*pbd.Field{{Name: "Sleeve", Id: 1}}, c)
+	if err == nil {
+		t.Errorf("Should have failed but did not")
+	}
+}
+
+func TestSleeveSuccess(t *testing.T) {
+	c := &pb.GramophileConfig{SleeveConfig: &pb.SleeveConfig{Mandate: pb.Mandate_RECOMMENDED,
+		AllowedSleeves: []*pb.Sleeve{{Name: "test"}}}}
+
+	err := ValidateConfig(context.Background(), []*pbd.Field{{Name: "Sleeve", Id: 1}}, c)
+	if err != nil {
+		t.Errorf("validate weight raised an error: %v", err)
+	}
+}
