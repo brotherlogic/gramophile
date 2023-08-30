fd09528fcded81019f2de0c51fab90c34d7f6a5f
brotherlogic <brotherlogic@gmail.com>
2023-08-30 06:39:15 -0700

    Adds new files to support arrived

---
+++ config/arrived.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"context"
+	"fmt"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ARRIVED_FIELD = "Arrived"
+)
+
+type arrived struct{}
+
+func (*arrived) Validate(ctx context.Context, fields []*pbd.Field, c *pb.GramophileConfig) error {
+	if c.GetArrivedConfig().GetMandate() != pb.Mandate_NONE {
+		found := false
+		for _, field := range fields {
+			if field.GetName() == ARRIVED_FIELD {
+				found = true
+			}
+		}
+		if !found {
+			return status.Errorf(codes.FailedPrecondition, fmt.Sprintf("Add a field called '%v'", ARRIVED_FIELD))
+		}
+	}
+
+	return nil
+}
---
+++ config/arrived_test.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"context"
+	"testing"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+)
+
+func TestArrived_FailedNoField(t *testing.T) {
+	c := &pb.GramophileConfig{ArrivedConfig: &pb.ArrivedConfig{Mandate: pb.Mandate_RECOMMENDED}}
+
+	err := ValidateConfig(context.Background(), []*pbd.Field{}, c)
+	if err == nil {
+		t.Errorf("Should have failed but did not (%v)", c)
+	}
+}
+
+func TestArrived_Success(t *testing.T) {
+	c := &pb.GramophileConfig{ArrivedConfig: &pb.ArrivedConfig{Mandate: pb.Mandate_RECOMMENDED}}
+
+	err := ValidateConfig(context.Background(), []*pbd.Field{{Name: "Arrived", Id: 1}}, c)
+	if err != nil {
+		t.Errorf("validate arrived raised an error: %v", err)
+	}
+}
