8e94302ba5c7dc34be4658bd5b09f9242e90cb93
Simon Tucker <brotherlogic@gmail.com>
2023-09-07 16:21:45 -0700

    Folder created on automatic mode. This closes #425

--- background/folders.go
+++ background/folders.go
@@ -3,12 +3,14 @@ package background
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/brotherlogic/discogs"
 	pb "github.com/brotherlogic/gramophile/proto"
 )
 
 func (b *BackgroundRunner) AddFolder(ctx context.Context, folderName string, d discogs.Discogs, u *pb.StoredUser) error {
+	log.Printf("Creating folder: %v", folderName)
 	folder, err := d.CreateFolder(ctx, folderName)
 	if err != nil {
 		return fmt.Errorf("unable to create folder: %w", err)
