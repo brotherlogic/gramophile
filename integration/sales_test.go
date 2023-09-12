f04e61b95cc13195580ac832bc5c7a94a076da90
brotherlogic <brotherlogic@gmail.com>
2023-09-12 07:03:33 -0700

    Working on ensuring that prices get adjusted in the sale loop. This closes #463

--- integration/sales_test.go
+++ integration/sales_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"context"
 	"testing"
 
 	"github.com/brotherlogic/discogs"
@@ -12,6 +13,28 @@ import (
 	"github.com/brotherlogic/gramophile/server"
 	rstore_client "github.com/brotherlogic/rstore/client"
 )
+
+func buildTestScaffold(t *testing.T) (context.Context, *server.Server, db.Database) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{&pbd.Folder{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	di := &discogs.TestDiscogsClient{
+		UserId: 123,
+		Fields: []*pbd.Field{{Id: 10, Name: "Keep"}},
+		Sales:  []*pbd.SaleItem{{ReleaseId: 123, SaleId: 12345}}}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := server.BuildServer(d, di, qc)
+
+	return ctx, s, d
+}
 
 func TestSyncSales_Success(t *testing.T) {
 	ctx := getTestContext(123)
@@ -55,4 +78,38 @@ func TestSyncSales_Success(t *testing.T) {
 	if sales.GetRecord().GetRelease().GetId() != 123 {
 		t.Fatalf("Bad record returned: %v", sales)
 	}
+}
+
+func TestSalesPriceIsAdjusted(t *testing.T) {
+	ctx, s, d := buildTestScaffold(t)
+
+	si := &pb.SaleInfo{
+		CurrentPrice: &pbd.Price{Value: 1234, Currency: "USD"},
+	}
+	err := d.SaveRecord(ctx, 123, &pb.Record{
+		Release: &pbd.Release{
+			Id:         123,
+			InstanceId: 1234,
+			FolderId:   12,
+			Labels:     []*pbd.Label{{Name: "AAA"}}},
+		SaleInfo: si,
+	})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveSale(ctx, 123, si)
+	if err != nil {
+		t.Fatalf("Can't save sale: %v", err)
+	}
+
+	s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			SaleConfig: &pb.SaleConfig{
+				Mandate:                pb.Mandate_REQUIRED,
+				HandlePriceUpdates:     pb.Mandate_REQUIRED,
+				UpdateFrequencySeconds: 10,
+				UpdateType:             pb.SaleUpdateType_MINIMAL_REDUCE,
+			},
+		},
+	})
 }
