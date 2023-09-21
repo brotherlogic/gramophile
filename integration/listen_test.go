7f0f21f594955b551046ead3080043a9debedad6
brotherlogic <brotherlogic@gmail.com>
2023-09-21 06:36:38 -0700

    Adds supporting test

---
+++ integration/listen_test.go
@@ -0,0 +1,131 @@
+package integration
+
+import (
+	"testing"
+	"time"
+
+	"github.com/brotherlogic/discogs"
+	pbd "github.com/brotherlogic/discogs/proto"
+	"github.com/brotherlogic/gramophile/background"
+	"github.com/brotherlogic/gramophile/db"
+	pb "github.com/brotherlogic/gramophile/proto"
+	queuelogic "github.com/brotherlogic/gramophile/queuelogic"
+	"github.com/brotherlogic/gramophile/server"
+	rstore_client "github.com/brotherlogic/rstore/client"
+)
+
+func TestSetListen_Success(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1234, FolderId: 12, Labels: []*pbd.Label{{Name: "AAA"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{&pbd.Folder{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "LastListenDate"}}}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := server.BuildServer(d, di, qc)
+
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			ListenConfig: &pb.ListenConfig{
+				Mandate: pb.Mandate_REQUIRED,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("unable to set config: %v", err)
+	}
+
+	_, err = s.SetIntent(ctx, &pb.SetIntentRequest{
+		Intent: &pb.Intent{
+			ListenTime: time.Now().Unix(),
+			NewScore:   5},
+		InstanceId: 1234,
+	})
+	if err != nil {
+		t.Fatalf("Error setting intent: %v", err)
+	}
+
+	//Run the intent
+	qc.FlushQueue(ctx)
+
+	resp, err := s.GetRecord(ctx, &pb.GetRecordRequest{
+		Request: &pb.GetRecordRequest_GetRecordWithId{
+			GetRecordWithId: &pb.GetRecordWithId{InstanceId: 1234}}})
+	if err != nil {
+		t.Fatalf("Bad record retrieve: %v", err)
+	}
+	rec := resp.GetRecord()
+	if rec.GetLastListenTime() == 0 || rec.Release.GetRating() != 5 {
+		t.Errorf("Listening was not set: %v", rec)
+	}
+}
+
+func TestSetListen_ResetScore(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{
+		InstanceId: 1234,
+		FolderId:   12,
+		Rating:     5,
+		Labels:     []*pbd.Label{{Name: "AAA"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{&pbd.Folder{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "LastListenDate"}}}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := server.BuildServer(d, di, qc)
+
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			ListenConfig: &pb.ListenConfig{
+				Mandate: pb.Mandate_REQUIRED,
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("unable to set config: %v", err)
+	}
+
+	_, err = s.SetIntent(ctx, &pb.SetIntentRequest{
+		Intent: &pb.Intent{
+			ListenTime: time.Now().Unix(),
+			NewScore:   -1},
+		InstanceId: 1234,
+	})
+	if err != nil {
+		t.Fatalf("Error setting intent: %v", err)
+	}
+
+	//Run the intent
+	qc.FlushQueue(ctx)
+
+	resp, err := s.GetRecord(ctx, &pb.GetRecordRequest{
+		Request: &pb.GetRecordRequest_GetRecordWithId{
+			GetRecordWithId: &pb.GetRecordWithId{InstanceId: 1234}}})
+	if err != nil {
+		t.Fatalf("Bad record retrieve: %v", err)
+	}
+	rec := resp.GetRecord()
+	if rec.GetLastListenTime() == 0 || rec.Release.GetRating() != 0 {
+		t.Errorf("Listening was not set (should have listen time and zero rating): %v", rec)
+	}
+}
