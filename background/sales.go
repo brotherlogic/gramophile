9b7ec28678706ac4ae2f429a57931b36d4612a1e
Simon Tucker <brotherlogic@gmail.com>
2023-10-03 10:44:44 -0700

    Logging

--- background/sales.go
+++ background/sales.go
@@ -24,6 +24,9 @@ func (b *BackgroundRunner) SyncSales(ctx context.Context, d discogs.Discogs, pag
 		log.Printf("found no sales :-(")
 	}
 	for _, sale := range sales {
+		if sale.GetStatus() == pbd.SaleStatus_FOR_SALE {
+			log.Printf("SALEITEM: %v", sale)
+		}
 		b.db.SaveSale(ctx, d.GetUserId(), &pb.SaleInfo{
 			SaleId:          sale.GetSaleId(),
 			LastPriceUpdate: time.Now().Unix(),
