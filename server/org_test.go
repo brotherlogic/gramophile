1d67b421a7582aa82d2f7017ca8314a7c39c53dc
brotherlogic <brotherlogic@gmail.com>
2023-10-12 06:23:02 -0700

    Working on artist grouping

--- server/org_test.go
+++ server/org_test.go
@@ -191,6 +191,99 @@ func TestLabelOrdering_WithOverrides(t *testing.T) {
 	}
 }
 
+func TestArtistOrdering_WithOverrides(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	di := &discogs.TestDiscogsClient{}
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1234, FolderId: 12, Ar: []*pbd.Label{{Id: 1, Name: "AAA"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1235, FolderId: 12, Labels: []*pbd.Label{{Id: 3, Name: "CCC"}, {Id: 2, Name: "BBB", Catno: "First"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1236, FolderId: 12, Labels: []*pbd.Label{{Id: 2, Name: "BBB", Catno: "Second"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{User: &pbd.User{DiscogsUserId: 123}, Auth: &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := Server{d: d, di: di, qc: qc}
+
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			OrganisationConfig: &pb.OrganisationConfig{
+				Organisations: []*pb.Organisation{
+					{
+						Name: "testing",
+						Grouping: &pb.Grouping{
+							Type: pb.GroupingType_GROUPING_GROUP_BY_LABEL,
+							LabelWeights: []*pb.LabelWeight{
+								{
+									Weight:  0.8,
+									LabelId: 2,
+								},
+							},
+						},
+						Foldersets: []*pb.FolderSet{
+							{
+								Name:   "testing",
+								Folder: 12,
+								Index:  1,
+								Sort:   pb.Sort_LABEL_CATNO,
+							}},
+						Spaces: []*pb.Space{
+							{
+								Name:         "Main Shelves",
+								Units:        1,
+								RecordsWidth: 100,
+							}},
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		t.Fatalf("Unable to set config: %v", err)
+	}
+
+	org, err := s.GetOrg(ctx, &pb.GetOrgRequest{OrgName: "testing"})
+	if err != nil {
+		t.Fatalf("Unable to get org: %v", err)
+	}
+
+	if len(org.GetSnapshot().GetPlacements()) != 3 {
+		t.Fatalf("Missing record in snapshot: %v", org)
+	}
+
+	bad := false
+	for _, o := range org.GetSnapshot().GetPlacements() {
+		if o.Index == 1 && o.Iid != 1234 {
+			bad = true
+		}
+		if o.Index == 2 && o.Iid != 1235 {
+			bad = true
+		}
+		if o.Index == 3 && o.Iid != 1236 {
+			bad = true
+		}
+	}
+
+	if bad {
+		t.Errorf("Bad placemen")
+		for _, o := range org.GetSnapshot().GetPlacements() {
+			t.Errorf("%v. %v -> %v", o.Index, o.Iid, o.SortKey)
+		}
+	}
+}
+
 func TestLooseLayoutSupport(t *testing.T) {
 	ctx := getTestContext(123)
 
