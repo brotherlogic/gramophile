8109565ed5a471cf29ee3d6e29ec5004a15a307a
brotherlogic <brotherlogic@gmail.com>
2023-10-19 06:45:29 -0700

    Setup scaffolding to support printing

--- integration/moves_test.go
+++ integration/moves_test.go
@@ -148,3 +148,90 @@ func TestRandomMoveHappensPostIntent(t *testing.T) {
 		t.Errorf("Did not find update: %v", r.GetRecord().GetUpdates())
 	}
 }
+
+func TestRandomMoveHappensPostIntent_WithPrint(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{
+			&pbd.Folder{Name: "Listening Pile", Id: 123},
+			&pbd.Folder{Name: "Limbo", Id: 125},
+		},
+		User: &pbd.User{DiscogsUserId: 123},
+		Auth: &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Arrived"}}}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := server.BuildServer(d, di, qc)
+
+	// Add a record that needs to be moved
+	d.SaveRecord(ctx, 123, &pb.Record{
+		Release: &pbd.Release{FolderId: 125, InstanceId: 1234},
+	})
+
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			CreateFolders: pb.Create_AUTOMATIC,
+			CreateMoves:   pb.Create_AUTOMATIC,
+			Moves: []*pb.FolderMove{
+				{
+					Origin:     pb.Create_MANUAL,
+					Name:       "test-move",
+					MoveFolder: "Listening Pile",
+					Criteria: &pb.MoveCriteria{
+						Arrived: pb.Bool_TRUE,
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("Unable to set config: %v", err)
+	}
+	qc.FlushQueue(ctx)
+
+	_, err = s.SetIntent(ctx, &pb.SetIntentRequest{
+		InstanceId: 1234,
+		Intent: &pb.Intent{
+			Arrived: time.Now().Unix(),
+		},
+	})
+	if err != nil {
+		t.Fatalf("unable to set intent: %v", err)
+	}
+	qc.FlushQueue(ctx)
+
+	r, err := s.GetRecord(ctx, &pb.GetRecordRequest{
+		IncludeHistory: true,
+		Request: &pb.GetRecordRequest_GetRecordWithId{
+			GetRecordWithId: &pb.GetRecordWithId{InstanceId: 1234}}})
+
+	if err != nil {
+		t.Fatalf("Unable to get record: %v", err)
+	}
+
+	if r.GetRecord() == nil || r.GetRecord().GetRelease().GetFolderId() != 123 {
+		t.Errorf("Record was not moved: %v", r.GetRecord())
+	}
+
+	found := false
+	for _, move := range r.GetRecord().GetUpdates() {
+		for _, exp := range move.GetExplanation() {
+			if exp == "Moved to Listening Pile following rule test-move" {
+				found = true
+			}
+		}
+	}
+
+	if !found {
+		t.Errorf("Did not find update: %v", r.GetRecord().GetUpdates())
+	}
+
+	if qc.GetPrintCount() != 1 {
+		t.Errorf("Print was not sent")
+	}
+}
