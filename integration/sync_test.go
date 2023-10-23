3ea8c60438c891ca6ada98fdb527df2061b946d3
brotherlogic <brotherlogic@gmail.com>
2023-10-23 16:26:17 -0700

    Added failing test of getting release

---
+++ integration/sync_test.go
@@ -0,0 +1,59 @@
+package integration
+
+import (
+	"testing"
+
+	"github.com/brotherlogic/discogs"
+	pbd "github.com/brotherlogic/discogs/proto"
+	"github.com/brotherlogic/gramophile/background"
+	"github.com/brotherlogic/gramophile/db"
+	pb "github.com/brotherlogic/gramophile/proto"
+	queuelogic "github.com/brotherlogic/gramophile/queuelogic"
+	"github.com/brotherlogic/gramophile/server"
+	rstore_client "github.com/brotherlogic/rstore/client"
+)
+
+func TestRecordUpdatedPostSync(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1234, FolderId: 12, Labels: []*pbd.Label{{Name: "AAA"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{&pbd.Folder{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Goal Folder"}}}
+
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := server.BuildServer(d, di, qc)
+
+	// Run a full update
+	qc.Enqueue(ctx, &pb.EnqueueRequest{
+		Element: &pb.QueueElement{
+			Auth:  "123",
+			Entry: &pb.QueueElement_RefreshCollection{},
+		},
+	})
+
+	qc.FlushQueue(ctx)
+
+	// Get the record
+	rec, err := s.GetRecord(ctx, &pb.GetRecordRequest{
+		Request: &pb.GetRecordRequest_GetRecordWithId{
+			GetRecordWithId: &pb.GetRecordWithId{InstanceId: 1234}}})
+
+	if err != nil {
+		t.Fatalf("Unable to get release: %v", err)
+	}
+
+	if rec.GetRecord().GetRelease().GetReleaseDate() != 12345678 {
+		t.Errorf("Record was not updated: %v", rec.GetRecord())
+	}
+}
