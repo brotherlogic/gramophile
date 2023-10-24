8a30a4df49d56ae71356a1b5b9013c4629b68bfa
Simon Tucker <brotherlogic@gmail.com>
2023-10-24 16:59:19 -0700

    Covers org diff

--- server/org_test.go
+++ server/org_test.go
@@ -106,6 +106,90 @@ func TestLabelOrdering(t *testing.T) {
 	}
 }
 
+func TestReleaseDateOrdering(t *testing.T) {
+	ctx := getTestContext(123)
+
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	di := &discogs.TestDiscogsClient{}
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1234, ReleaseDate: 1234, FolderId: 12, Labels: []*pbd.Label{{Name: "BBB"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1235, ReleaseDate: 1235, FolderId: 12, Labels: []*pbd.Label{{Name: "CCC"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1236, ReleaseDate: 1236, FolderId: 12, Labels: []*pbd.Label{{Name: "AAA"}}}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{User: &pbd.User{DiscogsUserId: 123}, Auth: &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := Server{d: d, di: di, qc: qc}
+
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{
+		Config: &pb.GramophileConfig{
+			OrganisationConfig: &pb.OrganisationConfig{
+				Organisations: []*pb.Organisation{
+					{
+						Name: "testing",
+						Foldersets: []*pb.FolderSet{
+							{
+								Name:   "testing",
+								Folder: 12,
+								Index:  1,
+								Sort:   pb.Sort_RELEASE_YEAR,
+							}},
+						Spaces: []*pb.Space{
+							{
+								Name:  "Main Shelves",
+								Units: 1,
+								Width: 100,
+							}},
+					},
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		t.Fatalf("Unable to set config: %v", err)
+	}
+
+	org, err := s.GetOrg(ctx, &pb.GetOrgRequest{OrgName: "testing"})
+	if err != nil {
+		t.Fatalf("Unable to get org: %v", err)
+	}
+
+	if len(org.GetSnapshot().GetPlacements()) != 3 {
+		t.Fatalf("Missing record in snapshot: %v", org)
+	}
+
+	bp := false
+	for _, o := range org.GetSnapshot().GetPlacements() {
+		if o.Index == 1 && o.Iid != 1234 {
+			bp = true
+		}
+		if o.Index == 2 && o.Iid != 1235 {
+			bp = true
+		}
+		if o.Index == 3 && o.Iid != 1236 {
+			bp = true
+		}
+	}
+
+	if bp {
+		t.Errorf("Bad placement")
+		for _, o := range org.Snapshot.GetPlacements() {
+			t.Errorf("%v. %v", o.Index, o.Iid)
+		}
+	}
+}
+
 func TestLabelOrdering_NoGroupingNoSpill(t *testing.T) {
 	ctx := getTestContext(123)
 
