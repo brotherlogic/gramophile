c9b8a01bda7776d68483c4bb6a05ac58fc50df04
Simon Tucker <brotherlogic@gmail.com>
2023-10-28 14:30:31 -0700

    More logging on updates

--- background/refreshrelease.go
+++ background/refreshrelease.go
@@ -4,11 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/brotherlogic/discogs"
 )
 
 func (b *BackgroundRunner) RefreshRelease(ctx context.Context, iid int64, d discogs.Discogs) error {
+	log.Printf("Refreshing %v", iid)
+
 	record, err := b.db.GetRecord(ctx, d.GetUserId(), iid)
 	if err != nil {
 		return fmt.Errorf("unable to get record from db: %w", err)
@@ -19,9 +22,9 @@ func (b *BackgroundRunner) RefreshRelease(ctx context.Context, iid int64, d disc
 		return fmt.Errorf("unable to get release %v from discogs: %w", record.GetRelease().GetId(), err)
 	}
 
-	log.Printf("Refreshing %v", iid)
-
 	// Update the release from the discogs pull
 	record.GetRelease().ReleaseDate = release.GetReleaseDate()
+	record.LastUpdateTime = time.Now().Unix()
+
 	return b.db.SaveRecord(ctx, d.GetUserId(), record)
 }
