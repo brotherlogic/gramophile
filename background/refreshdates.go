06da653f6a8515e172bad0cf72c51e4f01527011
brotherlogic <brotherlogic@gmail.com>
2023-10-30 06:43:06 -0700

    Added refresh logic

---
+++ background/refreshdates.go
@@ -0,0 +1,56 @@
+package background
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/brotherlogic/discogs"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+)
+
+func (b *BackgroundRunner) RefreshReleaseDates(ctx context.Context, d discogs.Discogs, token string, iid, mid int64, enqueue func(context.Context, *pb.EnqueueRequest) (*pb.EnqueueResponse, error)) error {
+	masters, err := d.GetMasterReleases(ctx, mid, 1, pbd.MasterSort_BY_YEAR)
+	if err != nil {
+		return err
+	}
+
+	for _, m := range masters {
+		_, err = enqueue(ctx, &pb.EnqueueRequest{
+			Element: &pb.QueueElement{
+				RunDate: time.Now().UnixNano(),
+				Auth:    token,
+				Entry: &pb.QueueElement_RefreshEarliestReleaseDate{
+					RefreshEarliestReleaseDate: &pb.RefreshEarliestReleaseDate{
+						Iid:          iid,
+						OtherRelease: m.GetId(),
+					}}},
+		})
+		if err != nil {
+			return fmt.Errorf("unable to queue sales: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (b *BackgroundRunner) RefreshReleaseDate(ctx context.Context, d discogs.Discogs, iid, rid int64) error {
+	release, err := d.GetRelease(ctx, rid)
+	if err != nil {
+		return err
+	}
+
+	storedRelease, err := b.db.GetRecord(ctx, d.GetUserId(), iid)
+	if err != nil {
+		return err
+	}
+
+	if release.GetReleaseDate() < storedRelease.GetEarliestReleaseDate() {
+		storedRelease.EarliestReleaseDate = release.GetReleaseDate()
+		return b.db.SaveRecord(ctx, d.GetUserId(), storedRelease)
+	}
+
+	return nil
+}
