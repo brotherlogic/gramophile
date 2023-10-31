d701ca7a033ef06c46a41c2084e03f85f62901ef
brotherlogic <brotherlogic@gmail.com>
2023-10-31 06:31:12 -0700

    Adds file

---
+++ background/background.go
@@ -0,0 +1,12 @@
+package background
+
+import "github.com/brotherlogic/gramophile/db"
+
+type BackgroundRunner struct {
+	db                    db.Database
+	key, secret, callback string
+}
+
+func GetBackgroundRunner(db db.Database, key, secret, callback string) *BackgroundRunner {
+	return &BackgroundRunner{db: db, key: key, secret: secret, callback: callback}
+}
