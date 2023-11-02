339677eb31c4b973b8498c03f70142a9a3d170e0
brotherlogic <brotherlogic@gmail.com>
2023-11-02 06:43:57 -0700

    Added failing test for getting wants

--- server/wantlist_test.go
+++ server/wantlist_test.go
@@ -13,6 +13,45 @@ import (
 	pb "github.com/brotherlogic/gramophile/proto"
 )
 
+func TestGetWantsFromWantlist(t *testing.T) {
+	ctx := getTestContext(123)
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Goal Folder"}}}
+	rstore := rstore_client.GetTestClient()
+	d := db.NewTestDB(rstore)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{&pbd.Folder{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Cannot save user: %v", err)
+	}
+	qc := queuelogic.GetQueue(rstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	s := Server{d: d, di: di, qc: qc}
+
+	_, err = s.AddWantlist(ctx, &pb.AddWantlistRequest{Name: "testing"})
+	if err != nil {
+		t.Fatalf("Unable to add wantlist: %v", err)
+	}
+
+	_, err = s.UpdateWantlist(ctx, &pb.UpdateWantlistRequest{
+		Name:  "testing",
+		AddId: 1234,
+	})
+	if err != nil {
+		t.Fatalf("Unable to update wantlist: %v", err)
+	}
+
+	// We should be able to identify 1234 in wants
+	wants, err := s.GetWants(ctx, &pb.GetWantsRequest{})
+	if err != nil {
+		t.Fatalf("Unable to get wants: %v", err)
+	}
+
+	if len(wants.GetWants()) == 0 {
+		t.Errorf("No wants listed")
+	}
+}
+
 func TestSaveAndLoadWantlist(t *testing.T) {
 	ctx := getTestContext(123)
 	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Goal Folder"}}}
