1a32e172cbd20e63e71fec7a4f5924bbca9f8875
Simon Tucker <brotherlogic@gmail.com>
2023-11-09 17:20:52 -0800

    Fixes bugs

--- integration/wantlist_test.go
+++ integration/wantlist_test.go
@@ -264,9 +264,9 @@ func TestWantlistUpdatedOnSync_HiddenAndInvisible(t *testing.T) {
 		t.Fatalf("Unable to get wants: %v", err)
 	}
 
-	if len(wants.GetWants()) != 2 ||
-		(wants.GetWants()[0].Id != 123 && wants.GetWants()[1].Id != 123) ||
-		(wants.GetWants()[0].State != pb.WantState_HIDDEN && wants.GetWants()[1].State != pb.WantState_HIDDEN) {
+	if len(wants.GetWants()) != 1 ||
+		wants.GetWants()[0].Id != 123 ||
+		wants.GetWants()[0].State != pb.WantState_WANTED {
 		t.Fatalf("Bad wants returned (expected to see original want): %v", wants)
 	}
 
