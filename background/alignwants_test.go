43dfd9d7060af44cd961b8310b6e462e228deab0
brotherlogic <brotherlogic@gmail.com>
2024-02-15 06:38:24 -0800

    Added a failing test

--- background/alignwants_test.go
+++ background/alignwants_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/brotherlogic/discogs"
+	pbd "github.com/brotherlogic/discogs/proto"
 	pb "github.com/brotherlogic/gramophile/proto"
 	"google.golang.org/grpc/metadata"
 )
@@ -18,14 +19,37 @@ func getTestContext(userid int) context.Context {
 
 func TestWantsDropped_Drop(t *testing.T) {
 	b := GetTestBackgroundRunner()
-	d := &discogs.TestDiscogsClient{}
+	d := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Cleaned"}}}
+	c := &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE}
 
 	ctx := getTestContext(123)
 
 	d.AddWant(ctx, 12345)
+	_, err := b.PullWants(ctx, d, 1, 12345, c)
+	if err != nil {
+		t.Fatalf("Unable to pull wants: %v", err)
+	}
 
-	b.AlignWants(ctx, d, &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE, Existing: pb.WantsExisting_EXISTING_DROP})
+	// The want should have been saved post pull
+	wants, err := b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
 
-	t.Errorf("Finish this")
+	if len(wants) != 1 || wants[0].Id != 12345 {
+		t.Errorf("Bad want pull: %v", wants)
+	}
+
+	b.AlignWants(ctx, d, c)
+
+	// Want should have been dropped
+	wants, err = b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
+
+	if len(wants) != 0 {
+		t.Errorf("Bad want pull post align: %v", wants)
+	}
 
 }
