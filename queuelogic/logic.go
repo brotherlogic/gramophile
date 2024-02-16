99b1a98a8e50ad02f5518ca61278eb174f9ed340
Simon Tucker <brotherlogic@gmail.com>
2024-02-16 07:32:41 -0800

    And clear this line

--- queuelogic/logic.go
+++ queuelogic/logic.go
@@ -241,7 +241,7 @@ func (q *Queue) Execute(ctx context.Context, req *pb.EnqueueRequest) (*pb.Enqueu
 }
 
 func (q *Queue) ExecuteInternal(ctx context.Context, d discogs.Discogs, u *pb.StoredUser, entry *pb.QueueElement) error {
-	log.Printf("Running queue entry: %v with %v", entry, u)
+	log.Printf("Running queue entry: %v", entry)
 	queueRun.With(prometheus.Labels{"type": fmt.Sprintf("%T", entry.Entry)}).Inc()
 	switch entry.Entry.(type) {
 	case *pb.QueueElement_MoveRecord:
