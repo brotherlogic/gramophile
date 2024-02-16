648b42c4f865f29977f3ff4d6f7e3f72eabfc4cf
brotherlogic <brotherlogic@gmail.com>
2024-02-15 16:36:38 -0800

    Working

--- background/alignwants.go
+++ background/alignwants.go
@@ -10,5 +10,38 @@ import (
 // Ensures everything is in a wantlist - only used when ORIGIN_GRAMOPHILE is set for wants
 func (b *BackgroundRunner) AlignWants(ctx context.Context, di discogs.Discogs, c *pb.WantsConfig) error {
 
+	wants, err := b.db.GetWants(ctx, di.GetUserId())
+	if err != nil {
+		return err
+	}
+
+	wantlists, err := b.db.GetWantlists(ctx, di.GetUserId())
+	if err != nil {
+		return err
+	}
+
+	cwantlist := &pb.Wantlist{Name: c.GetTransferList(), Type: pb.WantlistType_EN_MASSE}
+	for _, wl := range wantlists {
+		if wl.GetName() == c.GetTransferList() {
+			cwantlist = wl
+		}
+	}
+
+	for _, w := range wants {
+		found := false
+		for _, wl := range wantlists {
+			for _, entry := range wl.GetEntries() {
+				if entry.GetId() == w.GetId() {
+					found = true
+					break
+				}
+			}
+		}
+
+		if !found {
+			cwantlist.Entries = append(cwantlist.Entries, &pb.WantlistEntry{Id: w.GetId()})
+		}
+	}
+
 	return nil
 }
--- background/alignwants_test.go
+++ background/alignwants_test.go
@@ -20,7 +20,7 @@ func getTestContext(userid int) context.Context {
 func TestWantsDropped_Drop(t *testing.T) {
 	b := GetTestBackgroundRunner()
 	d := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Cleaned"}}}
-	c := &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE}
+	c := &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE, Existing: pb.WantsExisting_EXISTING_DROP}
 
 	ctx := getTestContext(123)
 
@@ -51,5 +51,97 @@ func TestWantsDropped_Drop(t *testing.T) {
 	if len(wants) != 0 {
 		t.Errorf("Bad want pull post align: %v", wants)
 	}
+}
 
+func TestWantsDropped_TransferToNew(t *testing.T) {
+	b := GetTestBackgroundRunner()
+	d := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Cleaned"}}}
+	c := &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE,
+		Existing: pb.WantsExisting_EXISTING_LIST, TransferList: "testing"}
+
+	ctx := getTestContext(123)
+
+	d.AddWant(ctx, 12345)
+	_, err := b.PullWants(ctx, d, 1, 12345, c)
+	if err != nil {
+		t.Fatalf("Unable to pull wants: %v", err)
+	}
+
+	// The want should have been saved post pull
+	wants, err := b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
+
+	if len(wants) != 1 || wants[0].Id != 12345 {
+		t.Errorf("Bad want pull: %v", wants)
+	}
+
+	b.AlignWants(ctx, d, c)
+
+	// Want should have been dropped
+	wants, err = b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
+
+	if len(wants) != 1 || wants[0].Id != 12345 {
+		t.Errorf("Bad want pull post align: %v", wants)
+	}
+
+	// Wantlist should have been created
+	wlist, err := b.db.GetWantlists(ctx, 123)
+	if err != nil {
+		t.Errorf("Error pulling wants: %v", err)
+	}
+	if len(wlist) != 1 || len(wlist[0].GetEntries()) != 1 || wlist[0].GetEntries()[0].Id == 12345 {
+		t.Errorf("Wantlist was not created or populated correctly: %v", wlist)
+	}
+}
+
+func TestWantsDropped_TransferToExisting(t *testing.T) {
+	b := GetTestBackgroundRunner()
+	d := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Cleaned"}}}
+	c := &pb.WantsConfig{Origin: pb.WantsBasis_WANTS_GRAMOPHILE,
+		Existing: pb.WantsExisting_EXISTING_LIST, TransferList: "testing"}
+	ctx := getTestContext(123)
+
+	b.db.SaveWantlist(ctx, 123, &pb.Wantlist{Name: "testing", Entries: []*pb.WantlistEntry{{Id: 111}, {Id: 12345}}})
+
+	d.AddWant(ctx, 12345)
+	_, err := b.PullWants(ctx, d, 1, 12345, c)
+	if err != nil {
+		t.Fatalf("Unable to pull wants: %v", err)
+	}
+
+	// The want should have been saved post pull
+	wants, err := b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
+
+	if len(wants) != 1 || wants[0].Id != 12345 {
+		t.Errorf("Bad want pull: %v", wants)
+	}
+
+	b.AlignWants(ctx, d, c)
+
+	// Want should have been dropped
+	wants, err = b.db.GetWants(ctx, 123)
+	if err != nil {
+		t.Fatalf("Unable to load wants: %v", err)
+	}
+
+	if len(wants) != 1 || wants[0].Id != 12345 {
+		t.Errorf("Bad want pull post align: %v", wants)
+	}
+
+	// Wantlist should have been created
+	list, err := b.db.GetWantlists(ctx, 123)
+	if err != nil {
+		t.Errorf("Error pulling wants: %v", err)
+	}
+	if len(list) != 1 || len(list[0].GetEntries()) != 2 || list[0].GetEntries()[1].Id != 12345 {
+		t.Errorf("Wantlist was not created or populated correctly: %v", list)
+	}
 }
