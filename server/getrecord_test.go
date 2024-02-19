d4c918e0e0183bf032d8c90c272d7fcf446fa2bc
Simon Tucker <brotherlogic@gmail.com>
2024-02-18 19:21:13 -0800

    Test coverage

--- server/getrecord_test.go
+++ server/getrecord_test.go
@@ -95,3 +95,30 @@ func TestRetrieveUpdates(t *testing.T) {
 		t.Errorf("No updates retreived, expected 1: %v", r.GetRecords()[0].GetRecord())
 	}
 }
+
+func TestGetSale(t *testing.T) {
+	ctx := getTestContext(123)
+
+	d := db.NewTestDB(rstore_client.GetTestClient())
+	err := d.SaveRecord(ctx, 123, &pb.Record{Release: &pbd.Release{InstanceId: 1234, FolderId: 12}})
+	if err != nil {
+		t.Fatalf("Can't init save record: %v", err)
+	}
+	err = d.SaveUser(ctx, &pb.StoredUser{User: &pbd.User{DiscogsUserId: 123}, Auth: &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+
+	err = d.SaveSale(ctx, 123, &pb.SaleInfo{SaleId: 12345, CurrentPrice: &pbd.Price{Value: 12345}})
+
+	s := Server{d: d}
+
+	sale, err := s.GetSale(ctx, &pb.GetSaleRequest{Id: 12345})
+	if err != nil {
+		t.Fatalf("Bad sale return %v", err)
+	}
+
+	if sale.GetSale().GetCurrentPrice().GetValue() != 12345 {
+		t.Errorf("Bad sale return: %v", sale)
+	}
+}
