d9601400cfcf2cd4894d3cf678232f4668e35ae9
Simon Tucker <brotherlogic@gmail.com>
2024-05-24 16:07:20 -0700

    Don't log empoty

--- queuelogic/logic.go
+++ queuelogic/logic.go
@@ -749,7 +749,6 @@ func (q *Queue) getNextEntry(ctx context.Context) (*pb.QueueElement, error) {
 		return strings.Compare(keys.GetKeys()[i], keys.GetKeys()[j]) < 0
 	})*/
 
-	log.Printf("Entries: %v", len(q.keys))
 	if len(q.keys) == 0 {
 		return nil, status.Errorf(codes.NotFound, "No queue entries")
 	}
