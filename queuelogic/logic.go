ead3e58f87114959418efe56726765d8c33e5f05
Simon Tucker <brotherlogic@gmail.com>
2024-05-25 15:44:39 -0700

    Also handle intents

--- queuelogic/logic.go
+++ queuelogic/logic.go
@@ -317,7 +317,7 @@ func (q *Queue) Execute(ctx context.Context, req *pb.EnqueueRequest) (*pb.Enqueu
 func (q *Queue) ExecuteInternal(ctx context.Context, d discogs.Discogs, u *pb.StoredUser, entry *pb.QueueElement) error {
 	log.Printf("Queue entry start: %v", entry)
 	queueRun.With(prometheus.Labels{"type": fmt.Sprintf("%T", entry.Entry)}).Inc()
-	if fmt.Sprintf("%T", entry.Entry) != "*proto.QueueElement_RefreshCollectionEntry" {
+	if fmt.Sprintf("%T", entry.Entry) != "*proto.QueueElement_RefreshCollectionEntry" && fmt.Sprintf("%T", entry.Entry) != "*proto.QueueElement_RefreshIntents" {
 		log.Printf("Skipping '%T'", entry.Entry)
 		return nil
 	}
