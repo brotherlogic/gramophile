ae1b786e46928567eb93437714d8f6c2091ef22a
Simon <brotherlogic@gmail.com>
2024-06-20 06:42:35 -0700

    Add token for script matcher

--- scripts/matcher.go
+++ scripts/matcher.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
 
 	"github.com/brotherlogic/goserver/utils"
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 
 	pbg "github.com/brotherlogic/gramophile/proto"
 	pbro "github.com/brotherlogic/recordsorganiser/proto"
@@ -17,6 +20,24 @@ import (
 func main() {
 	ctx, cancel := utils.ManualContext("syncing", time.Hour)
 	defer cancel()
+
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("No home dir: %v", err)
+	}
+
+	text, err := ioutil.ReadFile(fmt.Sprintf("%v/.gramophile", dirname))
+	if err != nil {
+		log.Fatalf("Failed to read token: %v", err)
+	}
+
+	user := &pbg.GramophileAuth{}
+	err = proto.UnmarshalText(string(text), user)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal: %v", err)
+	}
+
+	ctx = metadata.AppendToOutgoingContext(ctx, "auth-token", user.GetToken())
 
 	conn, err := utils.LFDialServer(ctx, "recordsorganiser")
 	if err != nil {
