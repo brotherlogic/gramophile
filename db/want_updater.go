cec594c670d55e061925ef2b9dfe26195e9d4cf0
Simon <brotherlogic@gmail.com>
2024-07-09 06:30:17 -0700

    Working on want update processing

--- db/want_updater.go
+++ db/want_updater.go
@@ -15,22 +15,24 @@ func (w *WantChanger) Name() string {
 	return "want_changer"
 }
 
-func (w *WantChanger) ProcessChange(ctx context.Context, c *pb.DBChange, config *pb.GramophileConfig) error {
+func (w *WantChanger) ProcessChange(ctx context.Context, c *pb.DBChange, user *pb.StoredUser) error {
 	// We only care about this change if it's a change record
 	if c.GetType() != pb.DBChange_CHANGE_WANT {
 		return nil
 	}
 
 	if c.GetOldWant().GetState() != c.GetNewWant().GetState() {
-		w.queue.Enqueue(ctx, &pb.EnqueueRequest{
+		_, err := w.queue.Enqueue(ctx, &pb.EnqueueRequest{
 			Element: &pb.QueueElement{
-				RunDate: time.Now().UnixNano() + int64(i),
+				RunDate: time.Now().UnixNano(),
 				Entry: &pb.QueueElement_RefreshWant{
 					RefreshWant: &pb.RefreshWant{Want: c.GetNewWant(), NewState: c.GetNewWant().GetState()},
 				},
-				Auth: entry.GetAuth(),
+				Auth: user.GetAuth().GetToken(),
 			},
 		})
+		return err
 	}
 
+	return nil
 }
