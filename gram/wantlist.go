7ed403187a71ccc0a7b81376d0f7f10da234cd2a
Simon Tucker <brotherlogic@gmail.com>
2024-09-17 14:19:41 -0700

    Fixing the wantlist score

--- gram/wantlist.go
+++ gram/wantlist.go
@@ -102,10 +102,19 @@ func executeWantlist(ctx context.Context, args []string) error {
 			return err
 		}
 
-		fmt.Printf("List: %v (%v)\n", wantlist.GetList().GetName(), wantlist.GetList().GetType())
+		total := float64(0)
+		count := float64(0)
+		for _, entry := range wantlist.GetList().GetEntries() {
+		    if entry.GetScore() > 0 {
+		    total += float64(entry.GetScore())
+		    count++
+		    }
+		    }
+		    
+		fmt.Printf("List: %v (%v) [%v]\n", wantlist.GetList().GetName(), wantlist.GetList().GetType(), total/count)
 		fmt.Printf("Updated: %v\n", time.Unix(0, wantlist.GetList().GetLastUpdatedTimestamp()))
 		for _, entry := range wantlist.GetList().GetEntries() {
-			fmt.Printf("  [%v] %v - %v (%v)\n", entry.GetId(), entry.GetArtist(), entry.GetTitle(), entry.GetState())
+			fmt.Printf("  [%v] %v - %v (%v) [%v]\n", entry.GetId(), entry.GetArtist(), entry.GetTitle(), entry.GetState(), entry.GetScore())
 		}
 	}
 
