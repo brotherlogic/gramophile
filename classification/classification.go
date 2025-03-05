4ce0573743839f5bf2e3d5bcf5fd9b761ce223c5
Simon <brotherlogic@gmail.com>
2025-03-05 14:56:30 -0800

    Rounds out application code for record classification

--- classification/classification.go
+++ classification/classification.go
@@ -39,6 +39,8 @@ func ApplyRule(rule *pb.ClassificationRule, record *pb.Record) bool {
 	switch rule.GetSelector().(type) {
 	case *pb.ClassificationRule_BooleanSelector:
 		return ApplyBooleanSelector(rule.GetBooleanSelector(), record)
+	case *pb.ClassificationRule_IntSelector:
+		return ApplyIntSelector(rule.GetIntSelector(), record)
 	}
 	return false
 }
@@ -49,6 +51,34 @@ func ApplyBooleanSelector(b *pb.BooleanSelector, r *pb.Record) bool {
 		field := fields.Get(i)
 		if field.TextName() == b.GetName() {
 			return r.ProtoReflect().Get(field).Bool()
+		}
+	}
+
+	return false
+}
+
+func compare(v, t int64, comp pb.Comparator) bool {
+	switch comp {
+	case pb.Comparator_COMPARATOR_GREATER_THAN:
+		return v > t
+	case pb.Comparator_COMPARATOR_LESS_THAN:
+		return v < t
+	case pb.Comparator_COMPARATOR_LESS_THAN_OR_EQUALS:
+		return v <= t
+	case pb.Comparator_COMPARATOR_GREATER_THAN_OR_EQUALS:
+		return v >= t
+	}
+
+	return false
+}
+
+func ApplyIntSelector(is *pb.IntSelector, r *pb.Record) bool {
+	fields := r.ProtoReflect().Descriptor().Fields()
+	for i := 0; i < fields.Len(); i++ {
+		field := fields.Get(i)
+		if field.TextName() == is.GetName() {
+			val := r.ProtoReflect().Get(field).Int()
+			return compare(val, is.GetThreshold(), is.Comp)
 		}
 	}
 
