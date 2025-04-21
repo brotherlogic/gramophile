6e193f942a95bebdfd8dae28ebe7a68c523f2d03
Simon <brotherlogic@gmail.com>
2025-04-21 06:44:57 -0700

    Adds basic config for adds / deletes. This closes #1600

---
+++ config/add.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"context"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+)
+
+var (
+	PURCHASED_PRICE    = "Purchase Price"
+	PURCHASED_LOCATION = "Purcahse Location"
+)
+
+type add struct{}
+
+func (*add) PostProcess(c *pb.GramophileConfig) (*pb.GramophileConfig, error) {
+	return c, nil
+}
+
+func (*add) GetClassification(c *pb.GramophileConfig) []*pb.Classifier {
+	return []*pb.Classifier{}
+}
+
+func (*arrived) Validate(ctx context.Context, fields []*pbd.Field, u *pb.StoredUser) error {
+	return nil
+}
