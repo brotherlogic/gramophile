ba5555265e6bf8a2e9759a023e26934a88eb6c9b
Simon Tucker <brotherlogic@gmail.com>
2026-04-13 13:13:50 -0700

    Fixes bug

--- db/db.go
+++ db/db.go
@@ -1064,7 +1064,9 @@ func (d *DB) GetUsers(ctx context.Context) ([]string, error) {
 	rusers := make(map[string]bool)
 	for _, key := range resp.GetKeys() {
 		elems := strings.Split(key, "/")
-		rusers[elems[3]] = true
+		if len(elems) >= 3 {
+			rusers[elems[2]] = true
+		}
 	}
 
 	return slices.Collect(maps.Keys(rusers)), err
