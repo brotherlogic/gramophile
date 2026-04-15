3b356c99f695f8dc5db0d8093fbeb6968e9fad7d
Simon Tucker <brotherlogic@gmail.com>
2026-04-15 06:42:27 -0700

    Fixed container

---
+++ postCreate.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install -y protobuf-compiler
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+
+# Account for Ghostty
+tic -x ghostty.terminfo
