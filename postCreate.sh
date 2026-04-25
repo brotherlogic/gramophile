af9df25ed36307a0b77a94236d03b45d4333db47
Brotherlogic Automation <brotherlogic-automation@gmail.com>
2026-04-25 00:04:50 +0000

    Modernize PR creation and fix automation user attribution. Part of #1933

--- postCreate.sh
+++ postCreate.sh
@@ -10,5 +10,5 @@ tic -x ghostty.terminfo
 # Install tmux and emacs
 sudo apt-get update && sudo apt-get install -y tmux emacs
 
-git config --global user.email 'brotherlogic-automation@gmail.com'
+git config --global user.email 'brotherlogic.automation@gmail.com'
 git config --global user.name 'Brotherlogic Automation'
