72cef602a892b1bfe1576f5e84224fa51e33ea53
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-05-02 04:30:09 +0000

    Address review feedback: remove exec for safety

--- postCreate.sh
+++ postCreate.sh
@@ -17,7 +17,7 @@ for RC_FILE in "$HOME/.zshrc" "$HOME/.bashrc"; do
 
 # Auto-attach to tmux session
 if [[ -z "$TMUX" ]] && [[ -z "$SKIP_TMUX" ]] && [[ -n "$PS1" ]] && [[ -t 0 ]]; then
-    exec tmux new-session -A -s default
+    tmux new-session -A -s default
 fi
 EOF
     fi
