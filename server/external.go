dd5eed8db9891cf80beb12dd4eb41b7156493075
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-06-18 16:12:02 -0700

    Address review feedback

--- server/external.go
+++ server/external.go
@@ -14,13 +14,13 @@ import (
 )
 
 func (s *Server) GetURL(ctx context.Context, req *pb.GetURLRequest) (*pb.GetURLResponse, error) {
-	s.loginMutex.Lock()
-	defer s.loginMutex.Unlock()
-
 	url, token, secret, err := s.di.GetLoginURL()
 	if err != nil {
 		return nil, fmt.Errorf("bad get for login ulr: %v", err)
 	}
+
+	s.loginMutex.Lock()
+	defer s.loginMutex.Unlock()
 
 	attempts, err := s.d.LoadLogins(ctx)
 	if err != nil {
@@ -49,10 +49,9 @@ func (s *Server) GetURL(ctx context.Context, req *pb.GetURLRequest) (*pb.GetURLR
 
 func (s *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*pb.GetLoginResponse, error) {
 	s.loginMutex.Lock()
-	defer s.loginMutex.Unlock()
-
 	attempts, err := s.d.LoadLogins(ctx)
 	if err != nil {
+		s.loginMutex.Unlock()
 		return nil, err
 	}
 
@@ -70,13 +69,15 @@ func (s *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*pb.Get
 	}
 
 	if foundAttempt != nil {
-		user, err := s.d.GenerateToken(ctx, foundAttempt.GetUserToken(), foundAttempt.GetUserSecret())
+		attempts.Attempts = newAttempts
+		err = s.d.SaveLogins(ctx, attempts)
+		s.loginMutex.Unlock()
+
 		if err != nil {
 			return nil, err
 		}
 
-		attempts.Attempts = newAttempts
-		err = s.d.SaveLogins(ctx, attempts)
+		user, err := s.d.GenerateToken(ctx, foundAttempt.GetUserToken(), foundAttempt.GetUserSecret())
 		if err != nil {
 			return nil, err
 		}
@@ -84,30 +85,31 @@ func (s *Server) GetLogin(ctx context.Context, req *pb.GetLoginRequest) (*pb.Get
 		// Enrich and store the user
 		log.Printf("from %v got %v and %v", foundAttempt, user, err)
 		sd := s.di.ForUser(&pbd.User{UserToken: foundAttempt.GetUserToken(), UserSecret: foundAttempt.GetUserSecret()})
-			duser, err := sd.GetDiscogsUser(ctx)
-			if err != nil {
-				return nil, err
-			}
-			user.User = duser
-			user.State = pb.StoredUser_USER_STATE_REFRESHING
+		duser, err := sd.GetDiscogsUser(ctx)
+		if err != nil {
+			return nil, err
+		}
+		user.User = duser
+		user.State = pb.StoredUser_USER_STATE_REFRESHING
 
-			// Trigger a low-pri collection update
-			s.qc.Enqueue(ctx, &pb.EnqueueRequest{
-				Element: &pb.QueueElement{
-					RunDate:          time.Now().UnixNano(),
-					Auth:             user.GetAuth().GetToken(),
-					BackoffInSeconds: 15,
-					Priority:         pb.QueueElement_PRIORITY_LOW,
-					Intention:        "New User Refresh",
-					Entry: &pb.QueueElement_RefreshCollectionEntry{
-						RefreshCollectionEntry: &pb.RefreshCollectionEntry{Page: 1},
-					},
+		// Trigger a low-pri collection update
+		s.qc.Enqueue(ctx, &pb.EnqueueRequest{
+			Element: &pb.QueueElement{
+				RunDate:          time.Now().UnixNano(),
+				Auth:             user.GetAuth().GetToken(),
+				BackoffInSeconds: 15,
+				Priority:         pb.QueueElement_PRIORITY_LOW,
+				Intention:        "New User Refresh",
+				Entry: &pb.QueueElement_RefreshCollectionEntry{
+					RefreshCollectionEntry: &pb.RefreshCollectionEntry{Page: 1},
 				},
-			})
+			},
+		})
 
 		return &pb.GetLoginResponse{Auth: user.GetAuth()}, s.d.SaveUser(ctx, user)
 	}
 
+	s.loginMutex.Unlock()
 	log.Printf("Tokens: %v", attempts)
 	return nil, status.Errorf(codes.DataLoss, "unable to locate token in db")
 }
