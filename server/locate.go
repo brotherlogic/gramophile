4ad191323acf65da303d669832fa51cd9e5312fd
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-06-30 18:10:32 -0700

    Implement LocateRecord logic
    Closes #2090

--- server/locate.go
+++ server/locate.go
@@ -2,12 +2,85 @@ package server
 
 import (
 	"context"
+	"sort"
 
 	pb "github.com/brotherlogic/gramophile/proto"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func (s *Server) LocateRecord(ctx context.Context, req *pb.LocateRecordRequest) (*pb.LocateRecordResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "not implemented")
+	user, err := s.getUser(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	records, err := s.d.GetRecords(ctx, user.GetUser().GetDiscogsUserId())
+	if err != nil {
+		return nil, err
+	}
+
+	var iids []int64
+	for _, recId := range records {
+		rec, err := s.d.GetRecord(ctx, user.GetUser().GetDiscogsUserId(), recId)
+		if err != nil {
+			return nil, err
+		}
+		if rec.GetRelease().GetId() == req.GetReleaseId() {
+			iids = append(iids, rec.GetRelease().GetInstanceId())
+		}
+	}
+
+	var locations []*pb.Location
+	for _, org := range user.GetConfig().GetOrganisationConfig().GetOrganisations() {
+		snapshot, err := s.d.GetLatestSnapshot(ctx, user.GetUser().GetDiscogsUserId(), org.GetName())
+		if err != nil {
+			continue // Ignore error if snapshot is not found
+		}
+		if snapshot == nil {
+			continue
+		}
+
+		// Ensure placements are sorted by index just in case
+		placements := snapshot.GetPlacements()
+		sort.Slice(placements, func(i, j int) bool {
+			return placements[i].GetIndex() < placements[j].GetIndex()
+		})
+
+		for idx, p := range placements {
+			for _, iid := range iids {
+				if p.GetIid() == iid {
+					loc := &pb.Location{
+						LocationName: org.GetName(),
+						Shelf:        p.GetSpace(),
+						Slot:         p.GetUnit(),
+					}
+					
+					// Get before context (up to 2)
+					beforeCount := 0
+					for i := idx - 1; i >= 0 && beforeCount < 2; i-- {
+						beforeP := placements[i]
+						loc.Before = append(loc.Before, &pb.Context{
+							Index: beforeP.GetIndex(),
+							Iid:   beforeP.GetIid(),
+						})
+						beforeCount++
+					}
+					
+					// Get after context (up to 2)
+					afterCount := 0
+					for i := idx + 1; i < len(placements) && afterCount < 2; i++ {
+						afterP := placements[i]
+						loc.After = append(loc.After, &pb.Context{
+							Index: afterP.GetIndex(),
+							Iid:   afterP.GetIid(),
+						})
+						afterCount++
+					}
+					locations = append(locations, loc)
+				}
+			}
+		}
+	}
+
+	return &pb.LocateRecordResponse{Locations: locations}, nil
 }
+
---
+++ server/locate_test.go
@@ -0,0 +1,83 @@
+package server
+
+import (
+	"testing"
+
+	pbd "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+	"github.com/brotherlogic/gramophile/db"
+	pstore_client "github.com/brotherlogic/pstore/client"
+)
+
+func TestLocateRecord_Success(t *testing.T) {
+	ctx := getTestContext(123)
+	pstore := pstore_client.GetTestClient()
+	d := db.NewTestDB(pstore)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		User: &pbd.User{DiscogsUserId: 123},
+		Auth: &pb.GramophileAuth{Token: "123"},
+        Config: &pb.GramophileConfig{
+			OrganisationConfig: &pb.OrganisationConfig{
+				Organisations: []*pb.Organisation{
+					{Name: "test-org"},
+				},
+			},
+		},
+	})
+	if err != nil {
+		t.Fatalf("cannot save user: %v", err)
+	}
+
+    s := Server{d: d}
+
+	err = d.SaveRecord(ctx, 123, &pb.Record{
+		Release: &pbd.Release{Id: 100, InstanceId: 1001},
+	})
+	if err != nil {
+		t.Fatalf("cannot save record: %v", err)
+	}
+    err = d.SaveRecord(ctx, 123, &pb.Record{
+		Release: &pbd.Release{Id: 101, InstanceId: 1002},
+	})
+    err = d.SaveRecord(ctx, 123, &pb.Record{
+		Release: &pbd.Release{Id: 100, InstanceId: 1003},
+	})
+
+	err = d.SaveSnapshot(ctx, &pb.StoredUser{
+		User: &pbd.User{DiscogsUserId: 123},
+	}, "test-org", &pb.OrganisationSnapshot{
+		Date: 12345,
+		Placements: []*pb.Placement{
+			{Iid: 1002, Space: "ShelfA", Unit: 1, Index: 1},
+			{Iid: 1001, Space: "ShelfA", Unit: 1, Index: 2},
+			{Iid: 1003, Space: "ShelfA", Unit: 1, Index: 3},
+		},
+	})
+	if err != nil {
+		t.Fatalf("cannot save snapshot: %v", err)
+	}
+
+	res, err := s.LocateRecord(ctx, &pb.LocateRecordRequest{
+		ReleaseId: 100,
+	})
+
+	if err != nil {
+		t.Fatalf("LocateRecord returned error: %v", err)
+	}
+
+	if len(res.GetLocations()) != 2 {
+		t.Fatalf("Expected 2 locations, got %v", len(res.GetLocations()))
+	}
+    
+    // Check first location (1001) which is at index 2 (middle)
+    loc1 := res.GetLocations()[0]
+    if loc1.GetShelf() != "ShelfA" || loc1.GetSlot() != 1 {
+        t.Errorf("Bad location 1: %v", loc1)
+    }
+    if len(loc1.GetBefore()) != 1 || loc1.GetBefore()[0].GetIid() != 1002 {
+        t.Errorf("Bad before context 1: %v", loc1.GetBefore())
+    }
+    if len(loc1.GetAfter()) != 1 || loc1.GetAfter()[0].GetIid() != 1003 {
+        t.Errorf("Bad after context 1: %v", loc1.GetAfter())
+    }
+}
