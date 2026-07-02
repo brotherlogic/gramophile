65808d963cde7eb59e2207ae20374d64c2a354d6
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-07-02 13:41:34 -0700

    Include artist name in locate CLI. Closes #2111

--- README.md
+++ README.md
@@ -13,5 +13,4 @@ Gramophile is built with Go.
 Current Go version: 1.26.2
 
 ## Features
-- LocateRecord: Provides functionality to query the location of records within the collection via the `gram locate` CLI command.
-
+- LocateRecord: Provides functionality to query the location of records within the collection via the `gram locate` CLI command, displaying the artist name along with the title.
--- gram/locate_test.go
+++ gram/locate_test.go
@@ -1,8 +1,65 @@
 package main
 
 import (
+	"context"
 	"testing"
+
+	discogs "github.com/brotherlogic/discogs/proto"
+	pb "github.com/brotherlogic/gramophile/proto"
+	"google.golang.org/grpc"
 )
+
+type mockClient struct {
+	pb.GramophileEServiceClient
+	records []*pb.RecordResponse
+}
+
+func (m *mockClient) GetRecord(ctx context.Context, in *pb.GetRecordRequest, opts ...grpc.CallOption) (*pb.GetRecordResponse, error) {
+	return &pb.GetRecordResponse{Records: m.records}, nil
+}
+
+func TestGetTitle_WithArtist(t *testing.T) {
+	client := &mockClient{
+		records: []*pb.RecordResponse{
+			{
+				Record: &pb.Record{
+					Release: &discogs.Release{
+						Title: "Some Title",
+						Artists: []*discogs.Artist{
+							{Name: "Some Artist"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	title := getTitle(context.Background(), client, 123)
+	expected := "Some Artist - Some Title"
+	if title != expected {
+		t.Errorf("Expected %v, got %v", expected, title)
+	}
+}
+
+func TestGetTitle_WithoutArtist(t *testing.T) {
+	client := &mockClient{
+		records: []*pb.RecordResponse{
+			{
+				Record: &pb.Record{
+					Release: &discogs.Release{
+						Title: "Some Title",
+					},
+				},
+			},
+		},
+	}
+
+	title := getTitle(context.Background(), client, 123)
+	expected := "Some Title"
+	if title != expected {
+		t.Errorf("Expected %v, got %v", expected, title)
+	}
+}
 
 func TestGetLocate(t *testing.T) {
 	module := GetLocate()
