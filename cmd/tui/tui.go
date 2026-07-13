2775ef59a6365b947af46bbbb60de336d983dd71
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-07-13 00:33:59 -0700

    Implement TUI login state and Discogs auth. Closes #2153

--- cmd/tui/tui.go
+++ cmd/tui/tui.go
@@ -1,10 +1,20 @@
 package tui
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
+	pb "github.com/brotherlogic/gramophile/proto"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/golang/protobuf/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 type appState int
@@ -23,7 +33,10 @@ type timeoutMsg struct{}
 const initialLogoDuration = 2 * time.Second
 
 type Model struct {
-	state appState
+	state      appState
+	url        string
+	loginToken string
+	err        error
 }
 
 func InitialModel() Model {
@@ -38,6 +51,96 @@ func (m Model) Init() tea.Cmd {
 	})
 }
 
+type loginURLMsg struct {
+	url   string
+	token string
+}
+
+type loginSuccessMsg struct {
+	auth *pb.GramophileAuth
+}
+
+type errMsg struct {
+	err error
+}
+
+type retryPollLoginMsg struct {
+	token string
+}
+
+func fetchLoginURLCmd() tea.Cmd {
+	return func() tea.Msg {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		conn, err := grpc.Dial("gramophile-grpc.brotherlogic-backend.com:80", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return errMsg{err: err}
+		}
+		defer conn.Close()
+
+		client := pb.NewGramophileEServiceClient(conn)
+		resp, err := client.GetURL(ctx, &pb.GetURLRequest{})
+		if err != nil {
+			return errMsg{err: err}
+		}
+
+		return loginURLMsg{url: resp.GetURL(), token: resp.GetToken()}
+	}
+}
+
+func pollLoginCmd(token string) tea.Cmd {
+	return func() tea.Msg {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		conn, err := grpc.Dial("gramophile-grpc.brotherlogic-backend.com:80", grpc.WithTransportCredentials(insecure.NewCredentials()))
+		if err != nil {
+			return errMsg{err: err}
+		}
+		defer conn.Close()
+
+		client := pb.NewGramophileEServiceClient(conn)
+		resp, err := client.GetLogin(ctx, &pb.GetLoginRequest{Token: token})
+		if err != nil {
+			return errMsg{err: err}
+		}
+
+		return loginSuccessMsg{auth: resp.GetAuth()}
+	}
+}
+
+func tickRetryPollLoginCmd(token string) tea.Cmd {
+	return tea.Tick(5*time.Second, func(t time.Time) tea.Msg {
+		return retryPollLoginMsg{token: token}
+	})
+}
+
+func saveToken(auth *pb.GramophileAuth) error {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	tmpFile := filepath.Join(dirname, ".gramophile.tmp")
+	finalFile := filepath.Join(dirname, ".gramophile")
+
+	f, err := os.OpenFile(tmpFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpFile)
+
+	err = proto.MarshalText(f, auth)
+	f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	return os.Rename(tmpFile, finalFile)
+}
+
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch m.state {
 	case StateStartupLogo:
@@ -45,11 +148,34 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyMsg:
 			// Any key press skips the logo
 			m.state = StateLogin
-			return m, nil
+			return m, fetchLoginURLCmd()
 		case timeoutMsg:
 			// Auto transition after timer
 			m.state = StateLogin
+			return m, fetchLoginURLCmd()
+		}
+	case StateLogin:
+		switch msg := msg.(type) {
+		case loginURLMsg:
+			m.url = msg.url
+			m.loginToken = msg.token
+			return m, pollLoginCmd(m.loginToken)
+		case loginSuccessMsg:
+			err := saveToken(msg.auth)
+			if err != nil {
+				m.err = err
+				return m, nil
+			}
+			m.state = StateLoadingSync
 			return m, nil
+		case errMsg:
+			if status.Code(msg.err) == codes.DataLoss {
+				return m, tickRetryPollLoginCmd(m.loginToken)
+			}
+			m.err = msg.err
+			return m, nil
+		case retryPollLoginMsg:
+			return m, pollLoginCmd(msg.token)
 		}
 	}
 	
@@ -76,7 +202,13 @@ func (m Model) View() string {
 		
 		return style.Render("GRAMOPHILE") + "\n\nPress any key to continue..."
 	case StateLogin:
-		return "Login Screen"
+		if m.err != nil && status.Code(m.err) != codes.DataLoss {
+			return fmt.Sprintf("Error logging in: %v\nPress q to quit", m.err)
+		}
+		if m.url == "" {
+			return "Fetching login URL..."
+		}
+		return fmt.Sprintf("Please open this URL in your browser to authenticate:\n\n%s\n\nWaiting for authorization...", m.url)
 	}
 	return "Gramophile TUI"
 }
--- cmd/tui/tui_test.go
+++ cmd/tui/tui_test.go
@@ -43,3 +43,40 @@ func TestTimerTransition(t *testing.T) {
 		t.Errorf("Expected state to transition to StateLogin on timeout, got %v", updatedModel.state)
 	}
 }
+
+type mockGetURLMsg struct {
+	url   string
+	token string
+}
+
+func TestLoginURLTransition(t *testing.T) {
+	m := InitialModel()
+	m.state = StateLogin
+
+	// When receiving a loginURLMsg, it should update the URL and token
+	msg := loginURLMsg{url: "http://test", token: "tok123"}
+	newModel, _ := m.Update(msg)
+	
+	updatedModel, _ := newModel.(Model)
+	if updatedModel.url != "http://test" {
+		t.Errorf("Expected url to be http://test, got %v", updatedModel.url)
+	}
+	if updatedModel.loginToken != "tok123" {
+		t.Errorf("Expected token to be tok123, got %v", updatedModel.loginToken)
+	}
+}
+
+func TestLoginSuccessTransition(t *testing.T) {
+	m := InitialModel()
+	m.state = StateLogin
+	m.loginToken = "tok123"
+
+	// When receiving a loginSuccessMsg, it should transition to StateLoadingSync
+	msg := loginSuccessMsg{auth: nil}
+	newModel, _ := m.Update(msg)
+	
+	updatedModel, _ := newModel.(Model)
+	if updatedModel.state != StateLoadingSync {
+		t.Errorf("Expected state to transition to StateLoadingSync on success, got %v", updatedModel.state)
+	}
+}
