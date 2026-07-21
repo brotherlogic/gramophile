0b98fedae0f290bc13f4c95d0307c9754d78a1bb
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-07-21 15:31:42 -0700

    Update TestOrganisation_OverlappingFolders to supply folders in user config

--- config/organisation_test.go
+++ config/organisation_test.go
@@ -67,30 +67,36 @@ func TestOrganisation_DuplicateName(t *testing.T) {
 }
 
 func TestOrganisation_OverlappingFolders(t *testing.T) {
-	c := &pb.StoredUser{Config: &pb.GramophileConfig{
-		WidthConfig: &pb.WidthConfig{Enabled: pb.Enabled_ENABLED_ENABLED},
-		OrganisationConfig: &pb.OrganisationConfig{
-			Organisations: []*pb.Organisation{
-				{
-					Name:    "testing1",
-					Density: pb.Density_WIDTH,
-					Foldersets: []*pb.FolderSet{
-						{Folder: 123},
+	c := &pb.StoredUser{
+		Folders: []*pbd.Folder{
+			{Id: 123, Name: "TestFolder"},
+		},
+		Config: &pb.GramophileConfig{
+			WidthConfig: &pb.WidthConfig{Enabled: pb.Enabled_ENABLED_ENABLED},
+			OrganisationConfig: &pb.OrganisationConfig{
+				Organisations: []*pb.Organisation{
+					{
+						Name:    "testing1",
+						Density: pb.Density_WIDTH,
+						Foldersets: []*pb.FolderSet{
+							{Folder: 123},
+						},
 					},
-				},
-				{
-					Name:    "testing2",
-					Density: pb.Density_WIDTH,
-					Foldersets: []*pb.FolderSet{
-						{Folder: 123},
+					{
+						Name:    "testing2",
+						Density: pb.Density_WIDTH,
+						Foldersets: []*pb.FolderSet{
+							{Folder: 123},
+						},
 					},
 				},
 			},
-		}}}
+		},
+	}
 
 	_, err := ValidateConfig(context.Background(), &pb.StoredUser{}, []*pbd.Field{{Name: "Arrived", Id: 1}, {Name: "Width", Id: 2}}, c)
-	if err == nil || (status.Code(err) != codes.FailedPrecondition && status.Code(err) != codes.InvalidArgument) {
-		t.Errorf("Should have failed with FailedPrecondition or InvalidArgument: %v", err)
+	if err == nil || status.Code(err) != codes.FailedPrecondition {
+		t.Errorf("Should have failed with FailedPrecondition: %v", err)
 	}
 }
 
