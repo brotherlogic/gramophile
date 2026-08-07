06bf84a6c5ca0713266cc9bd2c31967958afb4a1
Brotherlogic Automation <brotherlogic.automation@gmail.com>
2026-08-07 15:05:22 -0700

    test: verify SetConfig gRPC error propagation and queue enqueuing. Closes #2194 (#2202)

--- server/setconfig_test.go
+++ server/setconfig_test.go
@@ -11,6 +11,9 @@ import (
 	queuelogic "github.com/brotherlogic/gramophile/queuelogic"
 	pstore_client "github.com/brotherlogic/pstore/client"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
 	pb "github.com/brotherlogic/gramophile/proto"
 )
 
@@ -231,3 +234,133 @@ func TestConfigUpdate_CreateFolders(t *testing.T) {
 	}
 
 }
+
+func TestConfigUpdate_OrganisationValidationErrors(t *testing.T) {
+	ctx := getTestContext(123)
+
+	pstore := pstore_client.GetTestClient()
+	d := db.NewTestDB(pstore)
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Goal Folder"}}}
+	qc := queuelogic.GetQueue(pstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+
+	s := Server{d: d, di: di, qc: qc}
+
+	// 1. Missing width mandate error propagation (FailedPrecondition)
+	nconfig := &pb.GramophileConfig{
+		Basis: pb.Basis_GRAMOPHILE,
+		OrganisationConfig: &pb.OrganisationConfig{
+			Organisations: []*pb.Organisation{
+				{
+					Name:    "Org1",
+					Density: pb.Density_WIDTH,
+				},
+			},
+		},
+	}
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{Config: nconfig})
+	if err == nil {
+		t.Fatalf("Expected error for missing width mandate, got nil")
+	}
+	if status.Code(err) != codes.FailedPrecondition {
+		t.Errorf("Expected status code FailedPrecondition (%v), got: %v", codes.FailedPrecondition, status.Code(err))
+	}
+
+	// 2. Duplicate organisation name error propagation (AlreadyExists)
+	nconfig2 := &pb.GramophileConfig{
+		Basis: pb.Basis_GRAMOPHILE,
+		OrganisationConfig: &pb.OrganisationConfig{
+			Organisations: []*pb.Organisation{
+				{Name: "Org1"},
+				{Name: "Org1"},
+			},
+		},
+	}
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{Config: nconfig2})
+	if err == nil {
+		t.Fatalf("Expected error for duplicate org name, got nil")
+	}
+	if status.Code(err) != codes.AlreadyExists {
+		t.Errorf("Expected status code AlreadyExists (%v), got: %v", codes.AlreadyExists, status.Code(err))
+	}
+
+	// 3. Duplicate folder mapping error propagation (FailedPrecondition)
+	nconfig3 := &pb.GramophileConfig{
+		Basis: pb.Basis_GRAMOPHILE,
+		OrganisationConfig: &pb.OrganisationConfig{
+			Organisations: []*pb.Organisation{
+				{
+					Name: "Org1",
+					Foldersets: []*pb.FolderSet{
+						{Folder: 123},
+					},
+				},
+				{
+					Name: "Org2",
+					Foldersets: []*pb.FolderSet{
+						{Folder: 123},
+					},
+				},
+			},
+		},
+	}
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{Config: nconfig3})
+	if err == nil {
+		t.Fatalf("Expected error for duplicate folder mapping, got nil")
+	}
+	if status.Code(err) != codes.FailedPrecondition {
+		t.Errorf("Expected status code FailedPrecondition (%v), got: %v", codes.FailedPrecondition, status.Code(err))
+	}
+}
+
+func TestConfigUpdate_TriggersQueueOnValidConfig(t *testing.T) {
+	ctx := getTestContext(123)
+
+	pstore := pstore_client.GetTestClient()
+	d := db.NewTestDB(pstore)
+	di := &discogs.TestDiscogsClient{UserId: 123, Fields: []*pbd.Field{{Id: 10, Name: "Goal Folder"}}}
+	qc := queuelogic.GetQueue(pstore, background.GetBackgroundRunner(d, "", "", ""), di, d)
+	err := d.SaveUser(ctx, &pb.StoredUser{
+		Folders: []*pbd.Folder{{Name: "12 Inches", Id: 123}},
+		User:    &pbd.User{DiscogsUserId: 123},
+		Auth:    &pb.GramophileAuth{Token: "123"}})
+	if err != nil {
+		t.Fatalf("Can't init save user: %v", err)
+	}
+
+	s := Server{d: d, di: di, qc: qc}
+
+	nconfig := &pb.GramophileConfig{
+		Basis: pb.Basis_GRAMOPHILE,
+		OrganisationConfig: &pb.OrganisationConfig{
+			Organisations: []*pb.Organisation{
+				{
+					Name: "ValidOrg",
+					Foldersets: []*pb.FolderSet{
+						{Folder: 123},
+					},
+				},
+			},
+		},
+	}
+	_, err = s.SetConfig(ctx, &pb.SetConfigRequest{Config: nconfig})
+	if err != nil {
+		t.Fatalf("SetConfig failed on valid organisation config: %v", err)
+	}
+
+	elems, err := qc.List(ctx, &pb.ListRequest{})
+	if err != nil {
+		t.Fatalf("Unable to list queue elements: %v", err)
+	}
+
+	if len(elems.GetElements()) == 0 {
+		t.Errorf("Expected task queue elements to be enqueued on valid config submission, got 0 elements")
+	}
+}
+
